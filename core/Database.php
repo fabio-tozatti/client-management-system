e179dfd4496e0c7d67582703045ed3c41703b3b3
Fabio Cruz <fabiocruz@MacBook-Air-de-Fabio.local>
2024-08-31 16:56:46 -0300

    fix: corrige variaveis da string de conexão

--- core/Database.php
+++ core/Database.php
@@ -8,7 +8,7 @@ class Database
     {
         $dsn = "mysql:host={$host};dbname={$db};charset=utf8mb4";
         try {
-            $this->pdo = new PDO($dsn, 'root', '');
+            $this->pdo = new PDO($dsn, $user, $pass);
             $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         } catch (PDOException $e) {
             die("Database connection failed: " . $e->getMessage());
